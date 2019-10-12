c6bb890da8034f013cdb3f2209a01accefb9820e
Justin Dennis <jxd8037@g.rit.edu>
2019-10-12 12:28:55 -0400

    Entity class should be done for the most part

--- Strength/Assets/Scripts/Entity.cs
+++ Strength/Assets/Scripts/Entity.cs
@@ -2,29 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// List of all abilities in the game
+/// </summary>
 public enum Abilities
 {
-
+    SwordSwipe,
+    SwordSlam,
+    MagicMissile,
+    Lightning
 }
 
+[RequireComponent(typeof(Rigidbody2D))]
 public abstract class Entity : Interactable
 {
     [Header("Entity")]
     [SerializeField]
-    private float moveSpeed = 0;
+    protected float moveSpeed = 0;
     [SerializeField]
-    private float magicResistance = 0;
+    protected float magicResistance = 0;
     [SerializeField]
-    private float physicalResistance = 0;
+    protected float physicalResistance = 0;
     [SerializeField]
-    private float baseDamage = 0;
+    protected float baseDamage = 0;
     [SerializeField]
-    private List<Abilities> abilities;
+    protected List<Abilities> abilities;
+
+    protected new Rigidbody2D rigidbody;
+    protected Vector2 acceleration;
+
+    /// <summary>
+    /// The base damage of this entity used to affect ability damage
+    /// </summary>
+    public float BaseDamage { get; }
 
     /// <summary>
     /// The current direction of motion
     /// </summary>
-    private Vector2 Direction { get; set; }
+    protected Vector2 Direction { get; set; }
 
-    private float Velocity;
+    /// <summary>
+    /// The current velocity of the entity
+    /// </summary>
+    public Vector2 Velocity
+    {
+        get { return rigidbody.velocity; }
+        set { rigidbody.velocity = value; }
+    }
+
+    protected virtual void Update()
+    {
+        Velocity += acceleration * Time.deltaTime;
+        transform.position += (Vector3)Velocity * Time.deltaTime;
+    }
+
+    /// <summary>
+    /// Applies a force to the rigidbody
+    /// </summary>
+    /// <param name="force"></param>
+    protected void ApplyForce(Vector2 force)
+    {
+        acceleration += force / rigidbody.mass;
+    }
+
+    /// <summary>
+    /// Casts an ability using this entity's stats
+    /// </summary>
+    /// <param name="ability">The ability to cast</param>
+    protected abstract void Attack(Abilities ability);
 }
--- Strength/Assets/Scripts/Interactable.cs
+++ Strength/Assets/Scripts/Interactable.cs
@@ -7,10 +7,10 @@ public abstract class Interactable : MonoBehaviour
 {
     [Header("Interactable")]
     [SerializeField]
-    private int maxHealth = 0;
-    private int health = 0;
+    protected int maxHealth = 0;
+    protected int health = 0;
 
-    private Animator animator;
+    protected Animator animator;
 
     /// <summary>
     /// Decrements health by the damage of the attack taking damage type into account
@@ -18,7 +18,7 @@ public abstract class Interactable : MonoBehaviour
     /// <param name="attack"></param>
     public abstract void TakeDamage(Ability attack);
 
-    private void Awake()
+    protected virtual void Awake()
     {
         animator = GetComponent<Animator>();
     }
