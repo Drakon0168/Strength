9c38547ea2774c5e31ea3a93ce71285fd9295ba9
Justin Dennis <jxd8037@g.rit.edu>
2019-10-12 12:10:04 -0400

    Started working on entity

--- Strength/Assets/Scripts/Entity.cs
+++ Strength/Assets/Scripts/Entity.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Entity : MonoBehaviour
+public enum Abilities
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+}
+
+public abstract class Entity : Interactable
+{
+    [Header("Entity")]
+    [SerializeField]
+    private float moveSpeed = 0;
+    [SerializeField]
+    private float magicResistance = 0;
+    [SerializeField]
+    private float physicalResistance = 0;
+    [SerializeField]
+    private float baseDamage = 0;
+    [SerializeField]
+    private List<Abilities> abilities;
+
+    /// <summary>
+    /// The current direction of motion
+    /// </summary>
+    private Vector2 Direction { get; set; }
+
+    private float Velocity;
 }
