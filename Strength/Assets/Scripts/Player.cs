46f8791ecd2b79a6b4aaae8fe92a1d4f6c8edc38
Iain Davis (RIT Student) <icd1636@ad.rit.edu>
2019-10-12 12:29:39 -0400

    Update Player.cs

--- Strength/Assets/Scripts/Player.cs
+++ Strength/Assets/Scripts/Player.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private int mana;
+    private int stamina;
+    private int maxMana;
+    private int maxStamina;
+
     // Start is called before the first frame update
     void Start()
     {
