8e1f3ad025f89e7c29ac810dc974ef436cb8bcf8
iain.avatar <iain.avatar@gmail.com>
2019-10-13 11:39:58 -0400

    Shenanigans

--- Strength/Assets/Scripts/transition.cs
+++ Strength/Assets/Scripts/transition.cs
@@ -7,7 +7,7 @@ public class transition : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetBool("Transitioning", false);
+        animator.SetBool("Transitioning", true);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
