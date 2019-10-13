4defe86b33a753ccd23a2a0d3de6ab4f4b552fd6
iain.avatar <iain.avatar@gmail.com>
2019-10-12 22:54:08 -0400

    World Shenanigans

--- Strength/Assets/Scripts/World.cs
+++ Strength/Assets/Scripts/World.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class World : ScriptableObject
+public class World : MonoBehaviour
 {
     public enum WorldState
     {
         Physical,
         Magical
     }
-
+    private Canvas canvas;
     public WorldState wS = WorldState.Physical;
+
+    private void Awake()
+    {
+        canvas = FindObjectOfType<Canvas>();
+    }
 
     /// <summary>
     /// Changes the world state and handles all that logic
@@ -21,13 +26,15 @@ public class World : ScriptableObject
         if(wS == WorldState.Physical)
         {
             wS = WorldState.Magical;
-            FindObjectOfType<Canvas>().GetComponent<Image>().color = new Color(108, 47, 31);
+            canvas.GetComponent<Image>().color = new Color(108, 47, 31);
+            canvas.GetComponentInChildren<Animator>().SetTrigger("switchedToMagic");
             //TODO: Change screen color
         }
         else
         {
             wS = WorldState.Physical;
-            FindObjectOfType<Canvas>().GetComponent<Image>().color = new Color(0, 0, 0, 0);
+            canvas.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+            canvas.GetComponentInChildren<Animator>().SetTrigger("switchedToStrength");
             //TODO: Change screen color
         }
     }
