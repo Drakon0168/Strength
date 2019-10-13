7355f977677fffcb6b0514b465a3ad0616a052b7
iain.avatar <iain.avatar@gmail.com>
2019-10-13 10:50:22 -0400

    Disabled input during transition (WIP)

--- Strength/Assets/Scripts/Player.cs
+++ Strength/Assets/Scripts/Player.cs
@@ -18,6 +18,7 @@ public class Player : Entity
     private float minorCost;
     [SerializeField]
     private float majorCost;
+    public bool transitioning = false;
     private bool manaMajor = true;
     private bool physicalMajor = true;
     private State state = new State();
@@ -51,100 +52,102 @@ public class Player : Entity
     protected override void Update()
     {
         Vector2 direction = Vector2.zero;
-        if(Input.GetKey(KeyCode.W))
-        {
-            state = State.Move;
-            direction.y += 1;
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            state = State.Move;
-            direction.x -= 1;
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            state = State.Move;
-            direction.y -= 1;
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            state = State.Move;
-            direction.x += 1;
-        }
-
-        direction.Normalize();
-        Direction = direction;
-
-        if(direction == Vector2.zero)
-        {
-            Speed = 0;
-        }
-        else
-        {
-            Speed = moveSpeed;
-        }
-
-        ApplyForce((direction * moveSpeed) - Velocity);
-        base.Update();
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            if (world.wS == World.WorldState.Physical)
+        if (!transitioning) {
+            if (Input.GetKey(KeyCode.W))
             {
-                if (stamina >= minorCost)
-                {
-                    Attack(AbilityList.list[0]);
-                    stamina -= minorCost;
-                    mana += minorCost;
-                }
+                state = State.Move;
+                direction.y += 1;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                state = State.Move;
+                direction.x -= 1;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                state = State.Move;
+                direction.y -= 1;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                state = State.Move;
+                direction.x += 1;
+            }
+
+            direction.Normalize();
+            Direction = direction;
+
+            if (direction == Vector2.zero)
+            {
+                Speed = 0;
             }
             else
             {
-                if (mana >= minorCost)
-                {
-                    Attack(AbilityList.list[2]);
-                    mana -= minorCost;
-                    stamina += minorCost;
-                }
+                Speed = moveSpeed;
             }
-            BoolCheck();
-        }
-        else if (Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            if (world.wS == World.WorldState.Physical)
+
+            ApplyForce((direction * moveSpeed) - Velocity);
+            base.Update();
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                if (stamina >= majorCost && physicalMajor)
+                if (world.wS == World.WorldState.Physical)
                 {
-                    Attack(AbilityList.list[1]);
-                    stamina -= majorCost;
-                    mana += majorCost;
-                    physicalMajor = false;
+                    if (stamina >= minorCost)
+                    {
+                        Attack(AbilityList.list[0]);
+                        stamina -= minorCost;
+                        mana += minorCost;
+                    }
+                }
+                else
+                {
+                    if (mana >= minorCost)
+                    {
+                        Attack(AbilityList.list[2]);
+                        mana -= minorCost;
+                        stamina += minorCost;
+                    }
                 }
             }
-            else
+            else if (Input.GetKeyDown(KeyCode.Mouse1))
             {
-                if (mana >= majorCost && manaMajor)
+                if (world.wS == World.WorldState.Physical)
                 {
-                    Attack(AbilityList.list[3]);
-                    mana -= majorCost;
-                    stamina += majorCost;
-                    manaMajor = false;
+                    if (stamina >= majorCost && physicalMajor)
+                    {
+                        Attack(AbilityList.list[1]);
+                        stamina -= majorCost;
+                        mana += majorCost;
+                        physicalMajor = false;
+                    }
+                }
+                else
+                {
+                    if (mana >= majorCost && manaMajor)
+                    {
+                        Attack(AbilityList.list[3]);
+                        mana -= majorCost;
+                        stamina += majorCost;
+                        manaMajor = false;
+                    }
                 }
             }
-            BoolCheck();
-        }
         else if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Defense();
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftControl)){
-            Transform();
-        }
-        else if (Input.GetKeyDown(KeyCode.Escape))
-        {
+            {
+                Defense();
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftControl)) {
+                Transform();
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape))
+            {
 
+            }
         }
+        BoolCheck();
         UpdateUI();
+        transitioning = GetComponent<Animator>().GetBool("Transitioning");
     }
 
     /// <summary>
@@ -179,6 +182,7 @@ public class Player : Entity
         if(transformation != null)
         {
             transformation();
+            GetComponent<Animator>().SetTrigger("Transform");
         }
     }
 
