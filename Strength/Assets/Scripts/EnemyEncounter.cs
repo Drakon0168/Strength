c513269dec461d62a6a8b3ff04d260c5a4311db8
Eddie Brazier (RIT Student) <ecb9413@ad.rit.edu>
2019-10-13 15:23:56 -0400

    Added Enemy Encounter script and finished basic structure

---
+++ Strength/Assets/Scripts/EnemyEncounter.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEncounter : MonoBehaviour
+{
+    //field for a list of waves
+    [SerializeField]
+    private List<Vector4> encounter;
+
+    //field for a list of enemy gameObjects
+    [SerializeField]
+    private List<GameObject> enemyTypes;
+
+    //list of living enemies
+    private List<Enemy> livingEnemies;
+
+    //current wave number
+    private int waveNumber;
+
+    //field for the player character
+    [SerializeField]
+    private Player player;
+
+    //field for the minimum and maximum range of enemy spawning
+    [SerializeField]
+    private Vector2 spawnRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveNumber = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(livingEnemies.Count == 0)
+        {
+            CreateWave(encounter[waveNumber]);
+
+            waveNumber++;
+
+            if (waveNumber >= encounter.Count)
+            {
+                //TODO end encounter
+            }
+
+        }
+    }
+
+    private void CreateWave(Vector4 enemies)
+    {
+        //create temporary list to hold enemies for a specific wave
+        livingEnemies = new List<Enemy>();
+
+        //add specified number of golems to living enemy list
+        for(int i = 0; i < enemies.w; i++)
+        {
+            livingEnemies.Add(Instantiate(enemyTypes[0], GetRandomLocation(), Quaternion.identity).GetComponent<Enemy>());
+        }
+
+        //add specified number of dark nights to living enemy list
+        for (int i = 0; i < enemies.x; i++)
+        {
+            livingEnemies.Add(Instantiate(enemyTypes[1], GetRandomLocation(), Quaternion.identity).GetComponent<Enemy>());
+        }
+
+        //add specified number of dark mages to living enemy list
+        for (int i = 0; i < enemies.y; i++)
+        {
+            livingEnemies.Add(Instantiate(enemyTypes[2], GetRandomLocation(), Quaternion.identity).GetComponent<Enemy>());
+        }
+
+        //TODO add enemy four?
+        /*add specified number of E N E M Y  F O U R to living enemy list
+        for (int i = 0; i < enemies.z; i++)
+        {
+            livingEnemies.Add(Instantiate(enemyTypes[3], GetRandomLocation(), Quaternion.identity).GetComponent<Enemy>());
+        }
+        */
+    }
+
+    private Vector2 GetRandomLocation()
+    {
+        //range of enemy spawn
+        float enemyRange = Random.Range(spawnRange.x, spawnRange.y);
+
+        //angle of spawn
+        float spawnAngle = Random.Range(0, 360) * Mathf.Deg2Rad;
+
+        //Direction of spawn raycast
+        Vector2 spawnDirection = new Vector2(Mathf.Cos(spawnAngle), Mathf.Sin(spawnAngle));
+
+         RaycastHit2D[] spawnCast = Physics2D.RaycastAll(player.Location, spawnDirection, enemyRange);
+
+        foreach(RaycastHit2D spawnCheck in spawnCast)
+        {
+            //check if the raycast hit a wall, or else run the method again
+            /*
+            if(/*Raycast hit wall object)
+            {
+                return GetRandomLocation();
+            }
+            */
+        }
+
+        return player.Location + spawnDirection * spawnRange;
+    }
+}
