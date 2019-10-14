59a82d95eabe16dc0e8ba3ad664be86f2073463b
iain.avatar <iain.avatar@gmail.com>
2019-10-13 20:57:51 -0400

    Ults

---
+++ Strength/Assets/Scripts/Abilities/Ult/NightmareBlade.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Nightmare Blade", menuName = "Abilities/Ult/NightmareBlade")]
+public class NightmareBlade : Ult
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Strength/Assets/Scripts/Abilities/Ult/Ult.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ult", menuName = "Abilities/Ult/Ult")]
+public class Ult : Ability
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        foreach(Enemy e in enemies)
+        {
+            e.TakeDamage(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
